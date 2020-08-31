9c3fdf297f5ded228d9fe61096eed9e38db021c7
Dire-CPU <63191152+Dire-CPU@users.noreply.github.com>
2020-08-31 11:51:15 +0800

    Update data.py

--- main/data.py
+++ main/data.py
@@ -1,17 +1,17 @@
 ﻿#内网ip地址
-clientip=''
+clientip='192.168.1.1'
 
 #认证服务器地址
-nasip=''
+nasip='10.0.0.1'
 
 #mac地址，字母大写，使用-分割
-mac=''
+mac='5544458787'
 
 #安全码，无需修改
 secret='Eshore!@#'
 
 #用户名
-username=''
+username='07508668926'
 
 #密码
-password=''
+password='12345678'
